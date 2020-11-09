3058dd61e2d09a97d1dc2652d1f1a81d41700f6e
Emre Sağlam <emresaglam@gmail.com>
2020-11-08 19:40:34 -0800

    Added Old Blog Link to the sidebar. Also added favicon.ico (I think!)

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,66 +2,66 @@ var store = [{
         "title": "ý yerine ı görmek istiyor musunuz?",
         "excerpt":"Türkçe karakterlerin sorunsalı olan ISO-8859-9 formatıyla UTF-8 arasındaki uyuşmazlığı çözen küçük bir python scripti yazdım. Program girdi olarak ISO-8859-9 formatlanmış bir yazı alıyor ve de UTF-8 ile formatlayıp dosyaya yazıyor. Kullanımı: [code language=\"bash\"] python convertToUnicode.py --input /path/to/filename.srt --output /path/to/convertedfilename.srt [/code] ya da [code language=\"bash\"] python convertToUnicode.py -i /path/to/filename.srt -o /path/to/convertedfilename.srt...","categories": ["General"],
         "tags": ["linux","python","tools","türkçe","turkish"],
-        "url": "/general/y-yerine-i-gormek-istiyor-musunuz/",
-        "teaser": "/assets/img/emre.png"
+        "url": "http://localhost:4000/general/y-yerine-i-gormek-istiyor-musunuz/",
+        "teaser": "http://localhost:4000/assets/img/emre.png"
       },{
         "title": "Replacing Unity with XFCE in Ubuntu",
         "excerpt":"Here is what I do as soon as I install a new Ubuntu instance: I remove unity desktop and replace it with XFCE. And if you want to do the same it's pretty straightforward. ;) Perform all of these as root in a terminal window, or prepend with sudo. (Look...","categories": ["General"],
         "tags": ["2016","linux","short","tips","ubuntu"],
-        "url": "/general/replacing-unity-with-xfce-in-ubuntu/",
-        "teaser": "/assets/img/emre.png"
+        "url": "http://localhost:4000/general/replacing-unity-with-xfce-in-ubuntu/",
+        "teaser": "http://localhost:4000/assets/img/emre.png"
       },{
         "title": "Bloomsky weather data in pymultimonaprs",
         "excerpt":"I wrote a small python script to get the BloomSky weather data and \"air\" it on APRS using pymultimonaprs.  You can find it in my github repo. Read the README.md file on how to set it up.  &nbsp;  ","categories": ["General"],
         "tags": ["aprs","bloomsky","code","opensource","weather"],
-        "url": "/general/bloomsky-weather-data-in-pymultimonaprs/",
-        "teaser": "/assets/img/emre.png"
+        "url": "http://localhost:4000/general/bloomsky-weather-data-in-pymultimonaprs/",
+        "teaser": "http://localhost:4000/assets/img/emre.png"
       },{
         "title": "No timer in Mac OS X? No problem!",
         "excerpt":"Mac OS X doesn't come with a timer installed by default. There are bunch of timers in the App Store, you can definitely install one of those. But here is how to make one from scratch. Open terminal screen and type: sleep 5 ; say \"Time is up\" Well that's it...","categories": ["General"],
         "tags": ["command line","mac","mac OS X","terminal","tips"],
-        "url": "/general/no-timer-in-mac-os-x-no-problem/",
-        "teaser": "/assets/img/emre.png"
+        "url": "http://localhost:4000/general/no-timer-in-mac-os-x-no-problem/",
+        "teaser": "http://localhost:4000/assets/img/emre.png"
       },{
         "title": "Yaesu FTM-400XDR and Chirp",
         "excerpt":"I use Chirp. I love it. It's the only way to program my Baofeng UV-R(+) radios... Period. Also I love that I can download repeater settings according to a zip code, county, state, etc... I wanted to use Chirp for my Yaesu FTM-400XDR. However it looks like Chirp doesn't support...","categories": ["General"],
         "tags": ["ftm400","ftm400xdr","git","github","ham radio","python","tips","tricks","yaesu"],
-        "url": "/general/yaesu-ftm-400xdr-and-chirp/",
-        "teaser": "/assets/img/emre.png"
+        "url": "http://localhost:4000/general/yaesu-ftm-400xdr-and-chirp/",
+        "teaser": "http://localhost:4000/assets/img/emre.png"
       },{
         "title": "iCloud Photos, Meraki and Traffic Shaping",
         "excerpt":"I turned iCloud Photo Library on this week. We have close to 250 GB of photos, videos in several different computers and mobile devices. You might have guessed it: it flooded our network since our outbound internet peaks around 6 Mbs (realistically). I needed to do something. I have at...","categories": ["general"],
         "tags": ["apple","icloud","network","tips"],
-        "url": "/general/icloud-photos-meraki-and-traffic-shaping/",
-        "teaser": "/assets/img/Screen-Shot-2017-09-25-at-4.39.41-PM.png"
+        "url": "http://localhost:4000/general/icloud-photos-meraki-and-traffic-shaping/",
+        "teaser": "http://localhost:4000/assets/img/Screen-Shot-2017-09-25-at-4.39.41-PM.png"
       },{
         "title": "Air Quality Index Bot",
         "excerpt":"After the devastating Northern California wildfires, I found myself looking at AirNow multiple times a day. Also at work, like many Bay Area tech companies, we use HipChat (or Slack). Naturally I was inclined to issue a slash command to get the latest air quality index (AQI) for my area in...","categories": ["general"],
         "tags": ["api","git","github","opensource","project"],
-        "url": "/general/air_quality_index_bot/",
-        "teaser": "/assets/img/emre.png"
+        "url": "http://localhost:4000/general/air_quality_index_bot/",
+        "teaser": "http://localhost:4000/assets/img/emre.png"
       },{
         "title": "shadowsocks auto deploy",
         "excerpt":"I wrote a cloud-init script to automate the deployment of a shadowsocks server. I shared it in a gist here. Features: Creates a non-privileged user Uploads your ssh public key to the user Adds this user to the sudoers group Disables ssh login for the root user Downloads and installs...","categories": ["general","security"],
         "tags": ["linux","security"],
-        "url": "/general/security/shadowsocks-auto-deploy/",
-        "teaser": "/assets/img/emre.png"
+        "url": "http://localhost:4000/general/security/shadowsocks-auto-deploy/",
+        "teaser": "http://localhost:4000/assets/img/emre.png"
       },{
         "title": "Run pihole without raspberry pi",
         "excerpt":"I have few 24/7 running linux boxes at home. I wanted to try pi-hole. But I really didn’t want to add another computer (raspberry pi) on my network. So my initial feeling was why not try Docker? Good choice. Here is how easy it was. Pull pihole docker image $...","categories": ["general"],
         "tags": ["dns","docker","linux","pihole"],
-        "url": "/general/run-pihole-without-raspberry-pi/",
-        "teaser": "/assets/img/emre.png"
+        "url": "http://localhost:4000/general/run-pihole-without-raspberry-pi/",
+        "teaser": "http://localhost:4000/assets/img/emre.png"
       },{
         "title": "So you lost a disk on a MD RAID5 array, now what?",
         "excerpt":"It happens, you just lost a disk on your RAID5 MD array, or things are not how it should look like… How do we troubleshoot this? First things first, what’s the name of your MD device. You can easily learn that by issuing: cat /proc/mdstat This should output something similar...","categories": ["General"],
         "tags": ["linux","tips"],
-        "url": "/general/so-you-lost-a-disk-on-a-md-raid5-array-now-what/",
-        "teaser": "/assets/img/emre.png"
+        "url": "http://localhost:4000/general/so-you-lost-a-disk-on-a-md-raid5-array-now-what/",
+        "teaser": "http://localhost:4000/assets/img/emre.png"
       },{
         "title": "Apple watch radio",
         "excerpt":"If you’re like me and you have the radio app not working and your Apple Watch is saying: “Sign in to your Apple ID in General in the Apple Watch app on your iPhone” (And of course you are signed in), this solved my issue. Go to “Settings -&gt; iTunes...","categories": ["general"],
         "tags": ["apple","apple watch","tips","watch"],
-        "url": "/general/apple-watch-radio/",
-        "teaser": "/assets/img/black-and-silver-car-gear-shift-lever-437036.jpg"
+        "url": "http://localhost:4000/general/apple-watch-radio/",
+        "teaser": "http://localhost:4000/assets/img/black-and-silver-car-gear-shift-lever-437036.jpg"
       }]
